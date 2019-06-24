86f36e19203d72f633a2df9aeca98e74a3765d26
Michael Gregory <mgregory@cloudera.com>
2019-06-24 03:49:40 +0000

    fixed sub-path

--- utils/cdsw-build.sh
+++ utils/cdsw-build.sh
@@ -1,3 +1,3 @@
-pip3 install -r airline-sentiment/utils/requirements.txt
+pip3 install -r utils/requirements.txt
 python3 -m spacy download en
-#Rscript airline-sentiment/utils/install.R
+#Rscript utils/install.R
