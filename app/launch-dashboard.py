e0af6a4110ae04d538bba18d033faae35b1981d3
Michael Gregory <mgregory@cloudera.com>
2019-06-27 16:24:26 +0000

    changed joblib file name

--- app/launch-dashboard.py
+++ app/launch-dashboard.py
@@ -1,1 +1,10 @@
+from IPython.display import HTML
+from urllib.parse import urljoin
+
+HTML("<a href='https://{}.{}'>Airline Sentinment</a>".format(os.environ['CDSW_ENGINE_ID'],os.environ['CDSW_DOMAIN']))
+
+APP_URL = urljoin('https://'+os.environ['CDSW_ENGINE_ID']+'.'+os.environ['CDSW_DOMAIN'],'')
+
+print(APP_URL)
+
 !ipython3 app/dashboard.py
